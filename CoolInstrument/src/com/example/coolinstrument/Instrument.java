c01e6cdabb2029acb7f11b8b49e19fed3df6b55e
alex pan <ayp8@cornell.edu>
2013-12-22 21:41:34 -0500

    changes to music and layout
    4X4, correct sounds, and made the buttons swipe-enable after pressing
    the specific button

--- CoolInstrument/src/com/example/coolinstrument/Instrument.java
+++ CoolInstrument/src/com/example/coolinstrument/Instrument.java
@@ -18,14 +18,15 @@ import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Instrument extends Activity implements OnTouchListener {
+public class Instrument extends Activity {
 
-	Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
-	ArrayList<Button> buttonList = new ArrayList<Button>();
+	TextView b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15,
+			b16;
+	ArrayList<View> buttonList = new ArrayList<View>();
 	ArrayList<Integer> soundList = new ArrayList<Integer>();
 	ArrayList<MediaPlayer> mediaList = new ArrayList<MediaPlayer>();
 	SoundPool sp;
-	int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12 = 0;
+	int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16;
 	static int lock = 99;
 	MediaPlayer mp1, mp2, mp3;
 	private MediaPlayer mp;
@@ -37,18 +38,22 @@ public class Instrument extends Activity implements OnTouchListener {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.instrument);
-		b1 = (Button) findViewById(R.id.B1);
-		b2 = (Button) findViewById(R.id.B2);
-		b3 = (Button) findViewById(R.id.B3);
-		b4 = (Button) findViewById(R.id.B4);
-		b5 = (Button) findViewById(R.id.B5);
-		b6 = (Button) findViewById(R.id.B6);
-		b7 = (Button) findViewById(R.id.B7);
-		b8 = (Button) findViewById(R.id.B8);
-		b9 = (Button) findViewById(R.id.B9);
-		b10 = (Button) findViewById(R.id.B10);
-		b11 = (Button) findViewById(R.id.B11);
-		b12 = (Button) findViewById(R.id.B12);
+		b1 = (TextView) findViewById(R.id.B1);
+		b2 = (TextView) findViewById(R.id.B2);
+		b3 = (TextView) findViewById(R.id.B3);
+		b4 = (TextView) findViewById(R.id.B4);
+		b5 = (TextView) findViewById(R.id.B5);
+		b6 = (TextView) findViewById(R.id.B6);
+		b7 = (TextView) findViewById(R.id.B7);
+		b8 = (TextView) findViewById(R.id.B8);
+		b9 = (TextView) findViewById(R.id.B9);
+		b10 = (TextView) findViewById(R.id.B10);
+		b11 = (TextView) findViewById(R.id.B11);
+		b12 = (TextView) findViewById(R.id.B12);
+		b13 = (TextView) findViewById(R.id.B13);
+		b14 = (TextView) findViewById(R.id.B14);
+		b15 = (TextView) findViewById(R.id.B15);
+		b16 = (TextView) findViewById(R.id.B16);
 		test = (TextView) findViewById(R.id.tvTest);
 
 		buttonList.add(b1);
@@ -63,10 +68,14 @@ public class Instrument extends Activity implements OnTouchListener {
 		buttonList.add(b10);
 		buttonList.add(b11);
 		buttonList.add(b12);
+		buttonList.add(b13);
+		buttonList.add(b14);
+		buttonList.add(b15);
+		buttonList.add(b16);
 		for (int i = 0; i < buttonList.size(); i++) {
-			Button thisButton = buttonList.get(i);
+			View thisButton = buttonList.get(i);
 			// thisButton.setOnClickListener(this);
-			thisButton.setOnTouchListener(this);
+			// thisButton.setOnTouchListener(this);
 			thisButton.setSoundEffectsEnabled(false);
 		}
 
@@ -89,7 +98,10 @@ public class Instrument extends Activity implements OnTouchListener {
 		n11 = sp.load(this, R.raw.note11, 1);
 
 		n12 = sp.load(this, R.raw.note12, 1);
-
+		n13 = sp.load(this, R.raw.note13, 1);
+		n14 = sp.load(this, R.raw.note14, 1);
+		n15 = sp.load(this, R.raw.note15, 1);
+		n16 = sp.load(this, R.raw.note16, 1);
 		soundList.add(n1);
 
 		soundList.add(n2);
@@ -106,59 +118,74 @@ public class Instrument extends Activity implements OnTouchListener {
 		soundList.add(n10);
 		soundList.add(n11);
 		soundList.add(n12);
+		soundList.add(n13);
+		soundList.add(n14);
+		soundList.add(n15);
+		soundList.add(n16);
 
 	}
 
-	//button onclick gesture
+	// button onclick gesture
+
+	// swiping gesture
 	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		// TODO Auto-generated method stub
+	public boolean onTouchEvent(MotionEvent event) {
+		int action = event.getAction();
+		if (action == MotionEvent.ACTION_MOVE) {
 
-		test.setText("" + event.getActionMasked());
-		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			int i = buttonList.indexOf(v);
-			sp.play(soundList.get(i), 1, 1, 0, 0, 1);
-		}
-		return false;
-	}
+			float xVal = event.getX();
+			float yVal = event.getY();
 
-	
-	//swiping gesture
-	@Override
-	public boolean onTouchEvent(MotionEvent event)
-	{
-		int action=event.getAction();
-		if (action==MotionEvent.ACTION_MOVE){
-			
-			float xVal=event.getX();
-			float yVal=event.getY();
-
-			
 			int[] location = new int[2];
-			for (int buttonCounter=0;buttonCounter<buttonList.size();buttonCounter++){
-				if (lock!=buttonCounter){
-				View button=buttonList.get(buttonCounter);
-						button.getLocationOnScreen(location);
-				
-				float rectX=location[0];
-				float rectY=location[1];
-				RectF buttonRect=new RectF(rectX,rectY,rectX+button.getWidth(),rectY+button.getHeight());
-				Boolean breakout=false;
-				if (buttonRect.contains(xVal, yVal)){
-					sp.play(soundList.get(buttonCounter), 1, 1, 0, 0, 1);
-					breakout=true;
-				}
-				if (breakout){
-					lock=buttonCounter;
-					break;
-				}
+			for (int buttonCounter = 0; buttonCounter < buttonList.size(); buttonCounter++) {
+				if (lock != buttonCounter) {
+					View button = buttonList.get(buttonCounter);
+					button.getLocationOnScreen(location);
+
+					float rectX = location[0];
+					float rectY = location[1];
+					RectF buttonRect = new RectF(rectX, rectY, rectX
+							+ button.getWidth(), rectY + button.getHeight());
+					Boolean breakout = false;
+					if (buttonRect.contains(xVal, yVal)) {
+						sp.play(soundList.get(buttonCounter), 1, 1, 0, 0, 1);
+						breakout = true;
+					}
+					if (breakout) {
+						lock = buttonCounter;
+						break;
+					}
 				}
 			}
-			
-			
+
+		}
+		if (action == MotionEvent.ACTION_DOWN) {
+
+			float xVal = event.getX();
+			float yVal = event.getY();
+
+			int[] location = new int[2];
+			for (int buttonCounter = 0; buttonCounter < buttonList.size(); buttonCounter++) {
+				View button = buttonList.get(buttonCounter);
+				button.getLocationOnScreen(location);
+
+				float rectX = location[0];
+				float rectY = location[1];
+				RectF buttonRect = new RectF(rectX, rectY, rectX
+						+ button.getWidth(), rectY + button.getHeight());
+				Boolean breakout = false;
+				if (buttonRect.contains(xVal, yVal)) {
+					sp.play(soundList.get(buttonCounter), 1, 1, 0, 0, 1);
+					breakout = true;
+				}
+				if (breakout) {
+					lock = buttonCounter;
+					break;
+				}
+			}
+
 		}
 		return false;
-		
-	
+
 	}
 }
